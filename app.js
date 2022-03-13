aed2b7f8baf64f58e291f6ef68bb23efa654bb9c
Lawrence LCI <lcheunglci@gmail.com>
2022-03-12 23:56:53 -0800

    Add a car class

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello this works");
+class Car {
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine; 
+        this.color = color;
+    }
+
+    carStat() {
+        return `This car has ${this.doors} doors, a ${this.engine} engine and is the color ${this.color}`;
+    }
+}
+
+const cx5 = new Car(4, 'V6', 'grey');
+
+console.log(cx5);
+console.log(cx5.carStat());
