3a33211c5b65422ef9e4099cf187b59efe91088a
Lawrence LCI <lcheunglci@gmail.com>
2022-03-14 23:52:29 -0700

    Add includes example

---
+++ Arrays/includes.js
@@ -0,0 +1,6 @@
+const careers = ['teacher', 'doctor', 'engineer', 'nurse', 'scientist'];
+
+//Does the careers array include a nurse?
+
+// Note: it's case sensitive
+console.log(careers.includes('nurse'));
