9017f7e3e4967134adb231d5390a0652b0008c6a
Lawrence LCI <lcheunglci@gmail.com>
2022-03-14 23:27:31 -0700

    Add foreach example

---
+++ Arrays/forEach.js
@@ -0,0 +1,14 @@
+const scores = [50, 60, 70, 80, 90, 100];
+
+const logScore = (score) => console.log('Score Over 65', score);
+
+// Iterate over the scores array using forEach.
+// Execute the logScores
+
+// scores.forEach((element) => {console.log(element)})
+
+scores.forEach((element) => {
+    if (element > 65)
+      logScore(element);
+    }
+);
