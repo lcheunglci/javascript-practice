11fba62d3b834bdd7d863d1dd2ae04c6888f3225
Lawrence LCI <lcheunglci@gmail.com>
2022-03-16 00:12:22 -0700

    Add destructuring example

---
+++ ECMAScript6/destructure.js
@@ -0,0 +1,12 @@
+let [first, second,,,fifth] = [
+    'Vancouver',
+    'Calgary',
+    'Edmonton',
+    'Toronto',
+    'Surrey',
+    'Victoria'
+];
+
+console.log(first);
+console.log(second);
+console.log(fifth);
