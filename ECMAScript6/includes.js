ade5a40e0545ca8c7a73af8fc3a5f6048851fedb
Lawrence LCI <lcheunglci@gmail.com>
2022-03-16 00:13:53 -0700

    Add includes example

---
+++ ECMAScript6/includes.js
@@ -0,0 +1,11 @@
+let cities = [
+    'Vancouver',
+    'Calgary',
+    'Edmonton',
+    'Toronto',
+    'Surrey',
+    'Victoria'
+];
+
+console.log(cities.includes('Vancouver'));
+console.log(cities.includes('Seattle'));
