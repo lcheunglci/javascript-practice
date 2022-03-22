08dcd5073ac5b6009d86eaaabf851a2fd241b9a1
Lawrence LCI <lcheunglci@gmail.com>
2022-03-21 23:48:29 -0700

    Add arrow function example

---
+++ Functional/arrowFunctions.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+// function myFunction(arg1, arg2) {
+//     // function body;
+// }
+
+// const myOtherFunction = function(arg1, arg2) {
+//     // function body
+// }
+
+// const add = (x,y) => {
+//     const sum = x + y;
+//     return sum;
+// }
+
+// const add = (x,y) => {
+//     return x + y;
+// }
+
+const add = (x, y) => x + y;
+
+// const double = number => number * 2;
+
+console.log(add(4, 5));
