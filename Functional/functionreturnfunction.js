1807b5e7fc4d4a94cd0ae10975582fe8513867fa
Lawrence LCI <lcheunglci@gmail.com>
2022-03-23 23:48:32 -0700

    Add function that returns function example

---
+++ Functional/functionreturnfunction.js
@@ -0,0 +1,21 @@
+// const createPrinter = () => () => console.log('Hello');
+
+// const createPrinter = function () {
+//   return function () {
+//     console.log('Hello');
+//   };
+// };
+
+// const double = (x) => x * 2;
+// const triple = (x) => x * 3;
+// const quadruple = (x) => x * 4;
+
+const createMultiplier = (y) => (x) => x * y;
+
+const double = createMultiplier(2);
+const triple = createMultiplier(3);
+const quadruple = createMultiplier(4);
+
+double(3);
+triple(3);
+quadruple(3);
