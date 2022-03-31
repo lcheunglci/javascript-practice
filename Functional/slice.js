39d0761c0ba72a5fff94261b9ee53077a3042cbf
Lawrence LCI <lcheunglci@gmail.com>
2022-03-31 00:08:08 -0700

    Add slice example

---
+++ Functional/slice.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// slice with no arguments returns a copy of the origin array
+console.log(numbers.slice().reverse());
+console.log(numbers);
