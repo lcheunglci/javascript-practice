435e7def0b7d052ae91a9c667dfe75ef91b306a4
Lawrence LCI <lcheunglci@gmail.com>
2022-04-09 23:47:47 -0700

    Add mediator pattern example

---
+++ Patterns/Behavioral/mediator.js
@@ -0,0 +1,24 @@
+class TrafficTower {
+  constructor() {
+    this.airplanes = [];
+  }
+
+  requestPositions() {
+    return this.airplanes.map((airplane) => {
+      return airplane.position;
+    });
+  }
+}
+
+class Airplane {
+  constructor(position, trafficTower) {
+    this.position = position;
+    this.trafficTower = trafficTower;
+    this.trafficTower.airplanes.push(this);
+  }
+
+  // This is where the mediator pattern takes place.
+  requestPositions() {
+    return this.trafficTower.requestPositions();
+  }
+}
